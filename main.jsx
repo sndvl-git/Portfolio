92c56564d2f0a81f4c01ccec01d7dce45b6c01d3
sndvl-git <lanzerrol.sndvl@gmail.com>
2026-06-17 23:23:22 +0800

    update

--- main.jsx
+++ main.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
-import App from './index.jsx';
+import App from './App.jsx';
 
 const container = document.getElementById('root');
 if (!container) throw new Error('Root element not found');
